27676e89a01b0ac4efb0923a6ef68b62368c0da0
venkat <penubothuvenkannachowdary@gmail.com>
2019-11-15 14:02:46 +0530

    Working on the fetching and displaying data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
-import { Container, Header, Icon, Search, Grid, Select, Button } from 'semantic-ui-react';
+import { Container, Header, Icon, Search, Grid, Select, Button, Card, Image } from 'semantic-ui-react';
 import axios from 'axios';
 import './Home.css';
 
@@ -18,7 +18,9 @@ export default class Home extends Component {
       isLoading: false,
       results: [],
       value: '',
-      optionvalue: 'movie'
+      optionvalue: 'movie',
+      moviesfetched: [],
+      error: ''
   };
 
   componentDidMount() {
@@ -49,6 +51,39 @@ export default class Home extends Component {
 
   onOptionChange = (e, { value }) => this.setState({ optionvalue: value }, () => console.log(this.state.optionvalue))
 
+  onSearchHandler = () => {
+    console.log(this.state.optionvalue, this.state.value);
+    if(this.state.value !== '' && this.state.optionvalue !== '') {
+      axios.get('http://www.omdbapi.com/?type='+this.state.optionvalue+'&s='+this.state.value+'&apikey=5aebc1cf').then((response) => {
+     console.log(response.data);
+     if(response.data.Response === false) {
+         this.setState({
+           error: response.data.Error
+         })
+     }
+     else if(response.data.Search!==undefined) {
+        // const movies = response.data.Search.map((movie) => {
+        //    return ({
+        //        "key": movie.imdbID,
+        //        "title": movie.Title,
+        //        "rating": movie.imdbRating,
+        //        "genre": movie.Genre,
+        //        "image": movie.Poster,
+        //        "released": movie.Released,
+        //        "author": movie.Writer,
+        //        "actors": movie.Actors
+        //    })
+        // })
+        this.setState({
+          moviesfetched: response.data.Search
+        }, () => {
+          console.log(this.state.moviesfetched);
+        })
+     }
+   })
+    }
+  }
+
   handleSearchChange = (e, { value }) => {
     this.setState({ isLoading: true, value })
     setTimeout(() => {
@@ -66,7 +101,22 @@ export default class Home extends Component {
 
 
     render() {
-        const { isLoading, value, optionvalue, results } = this.state;
+        const { isLoading, value, optionvalue, results, error, moviesfetched } = this.state;
+
+        var moviesdata = error !== '' ? <h3>Movies Not Found!!!</h3> : <h1>Start Searching!!!</h1>
+
+        if (moviesfetched.length > 0) {
+         console.log('GReater')
+         const mvs = moviesfetched.map((movie) => {
+             return (
+               <Card key={movie.imdbID}>
+                 {movie.Poster === 'N/A' ? <Image src='https://react.semantic-ui.com/images/wireframe/white-image.png'></Image> : <Image src={movie.Poster} wrapped ui={false}/>}
+               </Card>
+             )
+          })
+          moviesdata = '';
+        var values = <Card.Group itemsPerRow={4}>{mvs}</Card.Group>
+        }
 
         return (
             <Container>
@@ -95,8 +145,12 @@ export default class Home extends Component {
         </Grid>
         <Grid centered columns={10}>
          <Grid.Column>
-          <Button basic color='green'>Search</Button>
+          <Button basic color='green' onClick={this.onSearchHandler}>Search</Button>
          </Grid.Column>
+        </Grid>
+        <Grid centered>
+         {moviesdata}
+         {values}
         </Grid>
        </Container>
         )
